aadfeab5291083570af2160a4b99601dfe73bb47
Lutfee Dorloh <dl.lutfee@gmail.com>
2026-02-23 10:02:41 +0700

    feat: Implement Angular code generator with database schema introspection and CSS framework rendering strategies.

--- obj/Debug/net8.0/Angular_Generate.AssemblyInfo.cs
+++ obj/Debug/net8.0/Angular_Generate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Angular_Generate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48d71fdd73c315ed8e5488e7a17e4e27b07727d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6ae93cf183ffe5853474fcaccfed0c9280776b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Angular_Generate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Angular_Generate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
